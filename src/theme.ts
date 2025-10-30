52fadd8adc377f4980975603b20f2a37b785c74f
Maksimov Yuriy <yura.maksimov.04@mail.ru>
2025-10-30 21:37:21 +0300

    Настройка тем и MUI

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#7fc980'
+    },
+    secondary: {
+      main: '#dc004e'
+    },
+  },
+});
+
+export default theme;
